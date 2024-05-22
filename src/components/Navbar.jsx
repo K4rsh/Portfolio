d3c1f78fcecf62eacb88904009c11e202de42c59
Utkarsh Upreti <utkarshupreti@Utkarshs-MacBook-Air.local>
2024-05-22 19:38:25 -0230

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import logo from '../assets/logo.png'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" style={{width: '50px', height: '50px'}} />
+      </div>
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <FaLinkedin />
+        <FaGithub />
+        <FaTwitter />
+        <FaInstagram />
+      </div>
+    </nav>
   )
 }
 
