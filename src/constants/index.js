3c0314871866552537a5b1dfd31cb23ee5127c3b
Utkarsh Upreti <utkarshupreti@Utkarshs-MacBook-Air.local>
2024-05-23 17:13:58 -0230

    All componnents added

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,74 +3,67 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer currently pursuing a Bachelor of Science in Computer Science at Memorial University of Newfoundland. With a strong foundation in Java, Python, Full Stack Web Development, Data Structures and Algorithms, Artificial Intelligence, Machine Learning, and Deep Learning, I am eager to apply my skills in real-world projects.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    period: "September 2023 - December 2024",
+    role: "Policy Analyst Assistant",
+    company: "Memorial University - Office of Equity, Diversity, Inclusion and Anti-Racism",
+    description: `Conducted research and reported findings about EDI-AR in Memorial University policies on workplace harassment, inclusion and disability. Assisted in marketing and organizing seminars, community programs and campaigns to take feedback of the university community on various issues such as inclusion, racism and diversity.`,
+    technologies: ["Research", "Reporting", "Community Engagement"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    period: "May 2023 - August 2023",
+    role: "Special Programs Coordinator",
+    company: "Memorial University - School of Graduate Studies",
+    description: `Communicated with the migration of university's older Scholarships website data to the new website, updating changes in the already present scholarship data and communicating any changes to the university authorities. Specialized in the use of excel spreadsheets and used various excel tools to represent data in a simpler and structured manner.`,
+    technologies: ["Data Migration", "Excel", "Communication"],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    period: "April 2022 - December 2022",
+    role: "Research Assistant",
+    company: "Memorial University - Department of Psychology",
+    description: `Used DeepLabCut, a python-based machine learning software, to record and analyze the movement of mice and help us with understanding how mice perceive the physical space around them. Analyzed Data using EthovisionXT-a video tracking system for automatically recording animals and their movement by placing markers on their heads and tails, which automatically tabulates all the collected data into an Excel Spreadsheet.`,
+    technologies: ["Python", "DeepLabCut", "Data Analysis"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Stock Market Emulation",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Acquired proficiency in HTML5, CSS3, and JavaScript for web development. Utilized React.js, Node.js and NoSQL database such as MongoDB to build dynamic and responsive websites. Making API calls to Stocks API named 'AlphaVantage' to get the real time value of stocks and performing buy and sell actions. Used hashing techniques (using bcrypt) and JSON Web token (jwtoken) to be sure of the safety and integrity of the Players, Users and Admin.",
+    technologies: ["HTML", "CSS", "JavaScript", "React", "Node.js", "MongoDB"],
   },
   {
-    title: "Task Management App",
+    title: "Machine Learning Data Analysis with Python",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Used Python for training and evaluating machine learning models such as Bottom-Line Linear Regression and Logistic Regression. Utilized data preparation techniques such as One-Hot encoding and Normalization to pre-process and analyze datasets.",
+    technologies: ["Python", "Machine Learning", "Data Analysis"],
   },
   {
-    title: "Portfolio Website",
+    title: "Can't Stop Game using Java",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+      "Collaborated on the development of the game using JFrame and JSwing frameworks. Designed and implemented graphical user interfaces (GUI) to enhance user experience. Contributed to the creation of interactive gameplay features and functionality.",
+    technologies: ["Java", "Swing"],
   },
   {
-    title: "Blogging Platform",
+    title: "Object Detection for Visually Impaired using Python",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "Developed an object detection system which uses CVLib and CV2 model. Utilizes any visual camera source to evaluate and identify the objects shown. Uses GTTS (google translate to speech) and Playsound frameworks to convert the textual feedback from the model into audio (using gTTS) and play it instantly (using playsound).",
+    technologies: ["Python", "CVLib", "CV2", "GTTS", "Playsound"],
   },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "St. John's, NL",
+  phoneNo: "(709) 986-7557",
+  email: "uupreti@mun.ca",
 };
